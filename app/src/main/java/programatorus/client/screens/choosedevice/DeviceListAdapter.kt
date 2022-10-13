999d037816d063c681517bb51796e0d1b37120ff
bzapa <bartlomiej.zapart@gmail.com>
2022-10-13 19:32:23 +0200

    rebase

---
+++ app/src/main/java/programatorus/client/screens/choosedevice/DeviceListAdapter.kt
@@ -0,0 +1,59 @@
+package programatorus.client.screens.choosedevice
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import programatorus.client.databinding.DeviceListItemBinding
+
+typealias ClickListener = (target: DeviceListItem) -> Unit
+
+class DeviceListAdapter():
+    RecyclerView.Adapter<DeviceListAdapter.DeviceItemViewHolder>() {
+
+    var devices: MutableList<DeviceListItem> = mutableListOf()
+
+    private var onItemClickListener: ClickListener = {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceItemViewHolder {
+        val binding = DeviceListItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false)
+        return DeviceItemViewHolder(binding)
+    }
+
+    fun setClickListener(clickListener: ClickListener) {
+        onItemClickListener = clickListener
+    }
+
+    override fun onBindViewHolder(holder: DeviceItemViewHolder, position: Int) {
+        holder.bind(devices[position], onItemClickListener)
+    }
+
+    override fun getItemCount() = devices.size
+
+    fun setItems(devices: MutableList<DeviceListItem>) {
+        this.devices = devices
+        notifyDataSetChanged()
+    }
+
+    fun addItem(device: DeviceListItem) {
+        devices.add(device)
+//        notifyItemInserted(devices.size - 1)
+        notifyDataSetChanged()
+    }
+
+    inner class DeviceItemViewHolder(private val binding: DeviceListItemBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: DeviceListItem, onItemClickListener: ClickListener) {
+            binding.deviceAddress.text = item.address
+            binding.deviceName.text = item.name
+
+            binding.root.setOnClickListener {
+                onItemClickListener(item)
+            }
+        }
+
+    }
+}
