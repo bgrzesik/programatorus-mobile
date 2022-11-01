851cab65d127bebeb8af1546dc4b2168e6acf41f
bzapa <bartlomiej.zapart@gmail.com>
2022-11-01 21:33:09 +0100

    add tabs

--- app/src/main/java/programatorus/client/screens/boards/all/AllBoardsListItem.kt
+++ app/src/main/java/programatorus/client/screens/boards/all/AllBoardsListItem.kt
@@ -1,3 +1,20 @@
 package programatorus.client.screens.boards.all
 
-data class AllBoardsListItem(val name: String, val isSelected: Boolean = false) {}
+import java.util.concurrent.atomic.AtomicBoolean
+
+data class AllBoardsListItem(val name: String, var isSelected: AtomicBoolean = AtomicBoolean(false)){
+
+    fun toggle(): Unit {
+        var temp: Boolean
+        do {
+            temp = isSelected.get()
+        } while (!isSelected.compareAndSet(temp, !temp))
+    }
+
+    companion object {
+        fun from(name: String, value: Boolean): AllBoardsListItem =
+            AllBoardsListItem(name, AtomicBoolean(value))
+    }
+}
+
+
