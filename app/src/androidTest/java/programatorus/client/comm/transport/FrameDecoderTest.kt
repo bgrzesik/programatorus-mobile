3d31a7ef07122638ca0f8674876a41f710249a2e
Bartłomiej Grzesik <bartlomiej.grzesik0@gmail.com>
2022-11-10 10:35:54 +0100

    Use serial like encoding for delimiting packets

---
+++ app/src/androidTest/java/programatorus/client/comm/transport/FrameDecoderTest.kt
@@ -0,0 +1,94 @@
+package programatorus.client.comm.transport
+
+import org.junit.Assert
+import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
+import java.util.*
+
+class FrameDecoderTest {
+
+    private fun decodeSingleFrame(bytes: ByteArray): ByteArray? {
+        val inputStream = ByteArrayInputStream(bytes)
+        val frameDecoder = FrameDecoder(inputStream)
+        return frameDecoder.readFrame()
+    }
+
+    private fun testSingleFrame(plain: ByteArray, byteStream: ByteArray) {
+        val decoded = decodeSingleFrame(byteStream)
+        Assert.assertNotNull(decoded)
+        Assert.assertArrayEquals(plain, decoded)
+    }
+
+    @Test
+    fun testWriteSliceSize() {
+        Assert.assertNull(decodeSingleFrame(byteArrayOf(0xff.toByte(), 0xf1.toByte(), 0x2f, 0x3f)))
+    }
+
+    @Test
+    fun testEmptySteam() {
+        Assert.assertNull(decodeSingleFrame(byteArrayOf()))
+    }
+
+    @Test
+    fun testNullSteam() {
+        Assert.assertNull(decodeSingleFrame(byteArrayOf(0x00, 0x00)))
+    }
+
+    @Test
+    fun testSteamWithoutZeros() {
+        testSingleFrame(
+            byteArrayOf(0x1),
+            byteArrayOf(0x00, 0x82.toByte(), 0x80.toByte(), 0x1, 0x0)
+        )
+        testSingleFrame(
+            byteArrayOf(0x1, 0x2),
+            byteArrayOf(0x00, 0x83.toByte(), 0x80.toByte(), 0x1, 0x2, 0x0)
+        )
+        testSingleFrame(
+            byteArrayOf(0x1, 0x2, 0x3),
+            byteArrayOf(0x00, 0x84.toByte(), 0x80.toByte(), 0x1, 0x2, 0x3, 0x0)
+        )
+    }
+
+    @Test
+    fun testSteamWithZeros() {
+        testSingleFrame(
+            byteArrayOf(0x1, 0x0),
+            byteArrayOf(0x00, 0x81.toByte(), 0x80.toByte(), 0x1, 0x0)
+        )
+        testSingleFrame(byteArrayOf(0x0), byteArrayOf(0x0, 0x80.toByte(), 0x80.toByte(), 0x0))
+        testSingleFrame(
+            byteArrayOf(0x1, 0x0),
+            byteArrayOf(0x0, 0x81.toByte(), 0x80.toByte(), 0x1, 0x0)
+        )
+        testSingleFrame(
+            byteArrayOf(0x1, 0x0, 0x1),
+            byteArrayOf(
+                0x0,
+                0x81.toByte(),
+                0x80.toByte(),
+                0x1,
+                0x82.toByte(),
+                0x80.toByte(),
+                0x1,
+                0x0
+            )
+        )
+        testSingleFrame(
+            byteArrayOf(0x1, 0x0, 0x1, 0x0),
+            byteArrayOf(
+                0x0,
+                0x81.toByte(),
+                0x80.toByte(),
+                0x1,
+                0x81.toByte(),
+                0x80.toByte(),
+                0x1,
+                0x0
+            )
+        )
+    }
+
+}
---
+++ app/src/main/java/programatorus/client/comm/transport/FrameEncoder.kt
@@ -0,0 +1,54 @@
+package programatorus.client.comm.transport
+
+import java.io.OutputStream
+import java.nio.ByteBuffer
+
+class FrameEncoder(
+    val mOutputStream: OutputStream
+) : OutputStream() {
+
+    private val mSliceBuffer = ByteBuffer.allocate(4096)
+
+    override fun write(b: Int) {
+        if (b != 0) {
+            mSliceBuffer.put(b.toByte())
+        } else {
+            writeSlice(false)
+        }
+    }
+
+    fun writeSliceSize(size: Int) {
+        var number = size;
+        assert(number <= 0x3fff)
+        mOutputStream.write(0x80 or (number and 0x7f))
+        number = number ushr 7
+        mOutputStream.write(0x80 or (number and 0x7f))
+    }
+
+    fun startFrame() {
+        reset()
+        mOutputStream.write(0);
+    }
+
+    private fun writeSlice(eof: Boolean) {
+        writeSliceSize(mSliceBuffer.position() + (if (eof) 1 else 0))
+        mOutputStream.write(mSliceBuffer.array(), 0, mSliceBuffer.position())
+        reset()
+    }
+
+    fun finishFrame() {
+        if (mSliceBuffer.position() > 0) {
+            writeSlice(true)
+        }
+        mOutputStream.write(0);
+        reset()
+    }
+
+    override fun flush() = mOutputStream.flush()
+
+    override fun close() = mOutputStream.close()
+
+    fun reset() {
+        mSliceBuffer.clear()
+    }
+}
