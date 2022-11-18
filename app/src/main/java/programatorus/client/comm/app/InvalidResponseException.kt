f6449151e08aae957a8649552286d641ff3e42a5
Bartłomiej Grzesik <bartlomiej.grzesik0@gmail.com>
2022-11-18 19:40:49 +0100

    Add FileUpload logic

--- app/src/main/java/programatorus/client/comm/app/InvalidResponseException.kt
+++ app/src/main/java/programatorus/client/comm/app/InvalidResponseException.kt
@@ -2,7 +2,10 @@ package programatorus.client.comm.app
 
 import programus.proto.Protocol.GenericMessage.PayloadCase
 
-data class InvalidResponseException(
-    val expectedPayloadCase: PayloadCase,
-    val actualPayloadCase: PayloadCase
-) : Exception("invalid response payload expected: $expectedPayloadCase actual: $actualPayloadCase")
+class InvalidResponseException(message: String) : Exception(message) {
+
+    constructor(
+        expectedPayloadCase: PayloadCase,
+        actualPayloadCase: PayloadCase
+    ): this("invalid response payload expected: $expectedPayloadCase actual: $actualPayloadCase")
+}
